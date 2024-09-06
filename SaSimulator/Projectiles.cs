deef5cdd851028260177db99ffaf3a35e1a82d17
KarelStehlik <karel.stehlik.cz@gmail.com>
2024-09-06 14:30:50 +0200

    lasers are now projectiles

--- SaSimulator/Projectiles.cs
+++ SaSimulator/Projectiles.cs
@@ -10,6 +10,7 @@ namespace SaSimulator
     internal class BulletTrail : GameObject
     {
         readonly Sprite sprite;
+        Time duration;
 
         public BulletTrail(Game game, Vector2 from, Vector2 to, Color color, Time duration) : base(game)
         {
@@ -43,8 +44,8 @@ namespace SaSimulator
         Speed speed, vx, vy;
         Time duration;
         float damage;
-        Color color;
-        Vector2 lastPosition;
+        protected Color color;
+        protected Vector2 lastPosition;
         private readonly int side;
 
         public Projectile(Game game, Transform transform, Speed speed, Time duration, float damage, int side, Color color) : base(game)
@@ -67,21 +68,11 @@ namespace SaSimulator
             {
                 foreach (ModuleHit module in target.RayIntersect(WorldPosition, speed * dt))
                 {
-                    IsDestroyed = true;
-                    if (game.hasGraphics)
+                    OnHit(target, module);
+                    if (IsDestroyed)
                     {
-                        WorldPosition = new(((double)module.position.X).Cells(), ((double)module.position.Y).Cells(), WorldPosition.rotation);
-                        DrawTrail();
+                        return;
                     }
-                    if (module.module.IsDestroyed)
-                    {
-                        target.GetNearestModule(module.position).TakeDamage(damage, DamageType.Ballistics);
-                    }
-                    else
-                    {
-                        module.module.TakeDamage(damage, DamageType.Ballistics);
-                    }
-                    return;
                 }
             }
             duration -= dt;
@@ -96,7 +87,26 @@ namespace SaSimulator
             WorldPosition = new(WorldPosition.x + vx * dt, WorldPosition.y + vy * dt, WorldPosition.rotation);
         }
 
-        private void DrawTrail()
+        protected virtual void OnHit(Ship target, ModuleHit module)
+        {
+            IsDestroyed = true;
+            if (game.hasGraphics)
+            {
+                WorldPosition = new(((double)module.position.X).Cells(), ((double)module.position.Y).Cells(), WorldPosition.rotation);
+                DrawTrail();
+            }
+            if (module.module.IsDestroyed)
+            {
+                target.GetNearestModule(module.position).TakeDamage(damage, DamageType.Ballistics);
+            }
+            else
+            {
+                module.module.TakeDamage(damage, DamageType.Ballistics);
+            }
+            return;
+        }
+
+        protected virtual void DrawTrail()
         {
             game.AddObject(new BulletTrail(game, lastPosition, WorldPosition.Position, color, 0.1.Seconds()));
             lastPosition = WorldPosition.Position;
@@ -110,56 +120,37 @@ namespace SaSimulator
     }
 
 
-    internal class Laser(Game game, Transform transform, Distance length, float damage, int side, Color color) : GameObject(game)
+    internal class Laser(Game game, Transform transform, Distance length, float damage, int side, Color color) :
+        Projectile(game, transform, length/1.Seconds(), 0.Seconds(), damage, side, color)
     {
-        Vector2 lastPosition = transform.Position;
-
         public override void Tick(Time dt)
         {
-            var targets = side == 0 ? game.player1Ships : game.player0Ships;
-            foreach (var target in targets)
-            {
-                foreach (ModuleHit module in target.RayIntersect(transform, length))
-                {
-                    if (game.hasGraphics)
-                    {
-                        WorldPosition = new(((double)module.position.X).Cells(), ((double)module.position.Y).Cells(), transform.rotation);
-                        DrawTrail();
-                    }
-                    if (module.module.IsDestroyed)
-                    {
-                        target.GetNearestModule(module.position).TakeDamage(damage, DamageType.Laser);
-                    }
-                    else
-                    {
-                        module.module.TakeDamage(damage, DamageType.Laser);
-                    }
-                    IsDestroyed = true;
-                    return;
-                }
-            }
-
-            if (game.hasGraphics)
-            {
-                transform += new Transform(length, 0.Cells(), 0);
-                DrawTrail();
-            }
-            IsDestroyed = true;
+            base.Tick(1.Seconds());
         }
 
-        private void DrawTrail()
+        protected override void DrawTrail()
         {
-            if (IsDestroyed)
-            {
-                return;
-            }
             game.AddObject(new BulletTrail(game, lastPosition, WorldPosition.Position, color, 0.01.Seconds()));
             lastPosition = WorldPosition.Position;
         }
 
-        public override void Draw(SpriteBatch batch)
+        protected override void OnHit(Ship target, ModuleHit module)
         {
-            DrawTrail();
+            IsDestroyed = true;
+            if (game.hasGraphics)
+            {
+                WorldPosition = new(((double)module.position.X).Cells(), ((double)module.position.Y).Cells(), WorldPosition.rotation);
+                DrawTrail();
+            }
+            if (module.module.IsDestroyed)
+            {
+                target.GetNearestModule(module.position).TakeDamage(damage, DamageType.Laser);
+            }
+            else
+            {
+                module.module.TakeDamage(damage, DamageType.Laser);
+            }
+            return;
         }
     }
 }
